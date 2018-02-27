0ee38f1d2103e55ded9a3258d62b31029e3f6ec8
cheyenneblue <32217561+cheyenneblue@users.noreply.github.com>
2018-02-27 07:38:24 +0300

    Add javascript.js
    And main, style

---
+++ javascript.js
@@ -0,0 +1,9 @@
+"use strict"
+
+
+!function($){
+   $(document).on('click', 'a[href^=\\#]', function () {
+        $('html, body').animate({ scrollTop:  $('a[name="'+this.hash.slice(1)+'"]').offset().top }, 1000 );
+        return false;
+    });
+}(jQuery);
